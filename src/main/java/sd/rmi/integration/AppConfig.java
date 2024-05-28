2ae29022b1848ff02fca5d3e33b4c70b20cb3612
gopoma <gordono@unsa.edu.pe>
2024-05-28 10:06:02 -0500

    Add: Se agregó AppConfig

---
+++ src/main/java/sd/rmi/integration/AppConfig.java
@@ -0,0 +1,20 @@
+package sd.rmi.integration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.remoting.rmi.RmiProxyFactoryBean;
+import org.springframework.remoting.rmi.RmiServiceExporter;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RmiServiceExporter rmiServiceExporter() throws Exception {
+        RmiServiceExporter exporter = new RmiServiceExporter();
+        exporter.setServiceName("CalculatorService");
+        exporter.setServiceInterface(CalculatorService.class);
+        exporter.setService(new CalculatorServiceImpl());
+        exporter.setRegistryPort(1099);
+        return exporter;
+    }
+}
