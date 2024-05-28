dcea17ca973fc68c2b019ad938b60d9604ac3cc7
bryanhancco <bhanccoco@unsa.edu.pe>
2024-05-28 10:32:56 -0500

    Agregando Server a RMI

---
+++ src/main/java/sd/rmi/integration/Server.java
@@ -0,0 +1,17 @@
+package sd.rmi.integration;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Server {
+
+    public static void main(String[] args) throws Exception {
+        CalculatorService calculatorService = new CalculatorServiceImpl();
+
+        Registry registry = LocateRegistry.createRegistry(1099);
+        registry.bind("CalculatorService", calculatorService);
+
+        System.out.println("Server started");
+    }
+
+}
