7f4f464cf9a7e5fce5a02719a095a04a221a98d7
fcardenasm <fcardenasm@unsa.edu.pe>
2024-05-28 08:35:23 -0500

    Add: implementacion de metodos add y subtract

--- src/main/java/sd/rmi/integration/CalculatorServiceImpl.java
+++ src/main/java/sd/rmi/integration/CalculatorServiceImpl.java
@@ -3,23 +3,19 @@ package sd.rmi.integration;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class CalculatorServiceImpl extends UnicastRemoteObject implements CalculatorService {
+public class CalculatorServiceImpl extends UnicastRemoteObject
+        implements CalculatorService {
 
-    protected CalculatorServiceImpl() throws RemoteException {
+    public CalculatorServiceImpl() throws RemoteException {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    @Override
     public int add(int a, int b) throws RemoteException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        return a + b;
     }
 
-    @Override
     public int subtract(int a, int b) throws RemoteException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'subtract'");
+        return a - b;
     }
 
 }
