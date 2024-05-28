b036b18c12dd4962f9bbe375cd4449401db62b44
Diego Pacori <dpacoria@unsa.edu.pe>
2024-05-28 10:51:03 -0500

    Se hizo uso de la instancia del servidor para realizar las funciones de resta y suma y se muestra en pantalla, también se muestra el error en caso ocurra

--- src/main/java/sd/rmi/integration/RMIClient.java
+++ src/main/java/sd/rmi/integration/RMIClient.java
@@ -6,8 +6,14 @@ public class RMIClient {
     public static void main(String[] args) {
         try{
             CalculatorService service = (CalculatorService) Naming.lookup("rmi://localhost/CalculatorService");
-        }catch(Exception e) {
 
+            int sumResult = service.add(5, 3);
+            int subtractResult = service.subtract(5, 3);
+
+            System.out.println("Resultado de la suma: " + sumResult);
+            System.out.println("Resultado de la resta: " + subtractResult);
+        }catch(Exception e) {
+            System.err.println("Error del cliente RMI: " + e);
         }
     }
 }
