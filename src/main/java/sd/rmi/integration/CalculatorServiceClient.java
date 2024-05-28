d996a315e47387474ac7ded4c69f872e58669d38
bryanhancco <bhanccoco@unsa.edu.pe>
2024-05-28 10:32:27 -0500

    Agregando ServiceClient a RMI

---
+++ src/main/java/sd/rmi/integration/CalculatorServiceClient.java
@@ -0,0 +1,22 @@
+package sd.rmi.integration;
+
+import java.rmi.RemoteException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceClient {
+
+    @Autowired
+    private CalculatorService calculatorService;
+
+    public int add(int a, int b) throws RemoteException {
+        return calculatorService.add(a, b);
+    }
+
+    public int subtract(int a, int b) throws RemoteException {
+        return calculatorService.subtract(a, b);
+    }
+
+}
